50a78b04f4f46893fb3feafef496346367f253f9
liuqiuchen <2016968116@qq.com>
2016-10-12 14:06:38 +0800

    Update models.py

--- zqxt_admin/blog/models.py
+++ zqxt_admin/blog/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# 兼容python2.x
+@python_2_unicode_compatible
 class Article(models.Model):
 	title = models.CharField(u'标题', max_length=256)
 	content = models.TextField(u'内容')
 
 	pub_date = models.DateTimeField(u'发表时间', auto_now_add=True, editable=True)
 	update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+	
+	def __str__(self):
+		# 显示自己的文章内容
+		return self.title
